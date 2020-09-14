2cf06291a2c0ab3cf49ceb02305168da881a48f2
Masinde Odera <masindeodera@gmail.com>
2020-09-14 11:14:14 +0200

    Updates

---
+++ precache-manifest.21198b8dd8fe171c5a5d93f4b4a43f9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382f7673dda104daf7c596f6f10c7681",
+    "url": "/beer-champ/index.html"
+  },
+  {
+    "revision": "1a2ad44f6568a5f5edf2",
+    "url": "/beer-champ/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "34fcb853ea1cf4219013",
+    "url": "/beer-champ/static/js/2.35830e75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/beer-champ/static/js/2.35830e75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a2ad44f6568a5f5edf2",
+    "url": "/beer-champ/static/js/main.b1bfc126.chunk.js"
+  },
+  {
+    "revision": "65ffbb65de88ad8ba804",
+    "url": "/beer-champ/static/js/runtime-main.35e5121e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/beer-champ/static/media/logo.5d5d9eef.svg"
+  }
+]);
